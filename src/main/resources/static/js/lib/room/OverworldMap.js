a6bf00bdbf623d7e90130c7c609f3c97c6d075b6
quizcoco <kunn0527@naver.com>
2024-04-17 21:45:16 +0900

    메인 room canvas

---
+++ src/main/resources/static/js/lib/room/OverworldMap.js
@@ -0,0 +1,54 @@
+class OverworldMap{
+    constructor(config){
+
+        this.gameObjects=config.gameObjects;
+        
+        this.lowerImage = new Image();//캐릭터가 서있는 바닥 이미지
+        this.lowerImage.src=config.lowerSrc;
+
+        this.upperImage = new Image(); //캐릭터 위 이미지(나무,지붕 등)
+        this.upperImage.src=config.upperSrc;
+
+    }
+
+    drawLowerImage(ctx){
+
+        ctx.drawImage(this.lowerImage,0,0);
+    }
+    drawUpperImage(ctx){
+
+        ctx.drawImage(this.upperImage,0,0);
+    }
+}
+window.OverworldMaps={ //각종맵객체
+    DemoRoom:{
+        lowerSrc:"/image/room/room2.png",
+        upperSrc:"/image/room/room2.png",
+        gameObjects:{
+            man1:new GameObject({
+                x:5,
+                y:2,
+            }),
+                npc1: new GameObject({
+                x:7,
+                y:10,
+                //src:"/image/"
+               })
+        }
+    },
+    // DemoRoom22:{
+    //     lowerSrc:"",
+    //     upperSrc:"",
+    //     gameObjects:{
+    //         man1:new GameObject({
+    //             x:5,
+    //             y:6,
+    //         }),
+    //             npc1: new GameObject({
+    //             x:7,
+    //             y:9,
+    //             //src:"/image/"
+    //            })
+    //     }
+    // }
+}
