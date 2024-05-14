9a1213107d1d580ad7db06e5d7a9608f6f378ac3
quizcoco <kunn0527@naver.com>
2024-05-14 21:45:06 +0900

    메인 벽만들기

--- src/main/resources/static/js/lib/room/GameObject.js
+++ src/main/resources/static/js/lib/room/GameObject.js
@@ -1,5 +1,7 @@
 class GameObject{
     constructor(config){
+        this.id=null;
+        this.isMounted=false;
         this.x=config.x||0;
         this.y=config.y||0;
         this.direction = config.direction || "down";
@@ -10,6 +12,11 @@ class GameObject{
 
 
         });
+    }
+
+    mount(map){
+        this.isMounted=true;
+        map.addWall(this.x,this.y);
     }
 
     update(){
--- src/main/resources/static/js/lib/room/OverworldMap.js
+++ src/main/resources/static/js/lib/room/OverworldMap.js
@@ -2,6 +2,7 @@ class OverworldMap{
     constructor(config){
 
         this.gameObjects=config.gameObjects;
+        this.walls = config.walls ||{};
         
         this.lowerImage = new Image();//캐릭터가 서있는 바닥 이미지
         this.lowerImage.src=config.lowerSrc;
@@ -20,6 +21,34 @@ class OverworldMap{
 
         ctx.drawImage(this.upperImage, utils.withGrid(10.5) - cameraPerson.x , utils.withGrid(6)-cameraPerson.y);
     }
+
+    isSpaceTaken(currentX,currentY,direction){
+        const{x,y} = utils.nextPosition(currentX,currentY,direction);
+        return this.walls[`${x},${y}`] || false;
+    }
+    mountObjects(){
+        Object.keys(this.gameObjects).forEach(key=>{
+
+            let object = this.gameObjects[key];
+            object.id=key;
+
+            object.mount(this);
+
+        })
+    }
+
+    addWall(x,y){
+        this.walls[`${x},${y}`] = true;
+    };
+    removeWall(x,y){
+        delete this.walls[`${x},${y}`]
+    }
+    moveWall(wasX,wasY,direction){
+        this.removeWall(wasX,wasY);
+        const{x,y} = utils.nextPosition(wasX,wasY,direction);//새로생성
+        this.addWall(x,y);
+    }
+
 }
 window.OverworldMaps={ //각종맵객체
     DemoRoom:{
@@ -36,21 +65,30 @@ window.OverworldMaps={ //각종맵객체
                 y:utils.withGrid(10),
                // src:"/image/room/cat-idle.png"
                })
+        },
+        walls:{
+            [utils.asGridCoord(2,4)]:true,
+            [utils.asGridCoord(3,4)]:true,
+            [utils.asGridCoord(4,4)]:true,
+            [utils.asGridCoord(5,4)]:true,
+            [utils.asGridCoord(6,4)]:true,
+
+        
         }
     },
-    // DemoRoom22:{
-    //     lowerSrc:"",
-    //     upperSrc:"",
-    //     gameObjects:{
-    //         man1:new GameObject({
-    //             x:5,
-    //             y:6,
-    //         }),
-    //             npc1: new GameObject({
-    //             x:7,
-    //             y:9,
-    //             //src:"/image/"
-    //            })
-    //     }
-    // }
+    battleField:{
+        lowerSrc:"",
+        upperSrc:"",
+        gameObjects:{
+            coco:new GameObject({
+                x:5,
+                y:6,
+            }),
+            insect: new GameObject({
+                x:7,
+                y:9,
+                //src:"/image/"
+               })
+        }
+    }
 }
