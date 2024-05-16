77359ca58cf2ffed9108bdb16f05da12dbc4de93
quizcocojy <jjioeq0935@gmail.com>
2024-05-16 20:57:37 +0900

    모바일 메뉴 클릭시 주변 어두워지는 효과 추가

--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -1,16 +1,24 @@
 let header = document.querySelector("#header");
 let mobileMenuIcon = header.querySelectorAll("nav.root-header-menu a");
 let mobileMenus = header.querySelector("nav.menu");
+let mobileOverlay = header.querySelector(".mobile-overlay");
 
 //====================메뉴아이콘 눌러 메뉴 여닫기================
 mobileMenuIcon[2].addEventListener("click",function(e){
     e.preventDefault();
     e.stopPropagation();
     
-    if(!mobileMenus.classList.contains("d:none"))
+    if(!mobileMenus.classList.contains("d:none")) {
+
         mobileMenus.classList.add("d:none");
-    else
+        mobileOverlay.classList.remove("active");
+    }
+
+    else {
+        
         mobileMenus.classList.remove("d:none");
+        mobileOverlay.classList.add("active");
+    }
 
 });
 // ====================메뉴 닫기=======================
@@ -19,8 +27,11 @@ document.addEventListener("click",function(e){
     if(mobileMenus.classList.contains("d:none"))
     return;
 
-    if(!mobileMenus.contains(e.target))
-    mobileMenus.classList.add("d:none");
+    if(!mobileMenus.contains(e.target)) {
+
+        mobileMenus.classList.add("d:none");
+        mobileOverlay.classList.remove("active");
+    }
 
     // e.preventDefault();
 })
