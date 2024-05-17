9550188e0ddd5ad74fb2bc7e19c899d646c3fb38
quizcocojy <jjioeq0935@gmail.com>
2024-05-17 23:26:08 +0900

    모바일 pc버전 화면 변경사항 추가

--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -12,7 +12,7 @@ mobileMenuIcon[2].addEventListener("click",function(e){
     if(!mobileMenus.classList.contains("d:none")) {
 
         mobileMenus.classList.add("d:none");
-        // 화면 원래대로 밝아짐
+        // 메뉴 외 화면 어둡게
         mobileOverlay.classList.remove("active");
         // 메뉴 아이콘 x모양으로 바뀜
         menuIcon.classList.remove("icon:x");
@@ -22,7 +22,7 @@ mobileMenuIcon[2].addEventListener("click",function(e){
     else {
         
         mobileMenus.classList.remove("d:none");
-         // 메뉴 외 화면 어둡게
+        // 화면 원래대로 밝아짐
         mobileOverlay.classList.add("active");
         // 메뉴 아이콘 리스트 모양으로 바뀜
         menuIcon.classList.remove("icon:list");
@@ -45,11 +45,22 @@ document.addEventListener("click",function(e){
     }
 
     // e.preventDefault();
+
 });
 
-//=============화면 크기 변화 감지해서 밝은 상태로============
+//=============화면 크기 변화 감지============
 window.addEventListener("resize", ()=> {
+
+    // 모바일에서 pc화면으로 바뀔때 메뉴가 열려 있었어도 배경 밝아지도록
     if(window.innerWidth >= 1200) {
         mobileOverlay.classList.remove("active");
+    }
+    
+    // pc화면에서 모바일으로 바뀔때 메뉴가 열려 있었다면 배경 어두워지도록
+    if(window.innerWidth < 1200 
+                             && 
+                            !mobileMenus.classList.contains("d:none")){
+       
+        mobileOverlay.classList.add("active");
     }
 });
