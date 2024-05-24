81ead23b9ec4cae724bb6f6e68d3d23c24792ac6
quizcocojy <jjioeq0935@gmail.com>
2024-05-24 18:02:05 +0900

    퀴즈검색 모달 설정 추가

--- src/main/resources/static/js/study/search.js
+++ src/main/resources/static/js/study/search.js
@@ -1,11 +1,14 @@
 /* study search index====================================== */
 
+const searchQuiz = document.getElementById("search-quiz");
 let randQuizBtn = document.getElementById("rand-quiz");
 let randModal = document.getElementById("r-modal");
+let modalBackDrop = searchQuiz.querySelector(".backdrop");
 
 // 오늘올라온문제 디테일 모달
 randQuizBtn.addEventListener("click", () => {
     randModal.classList.remove('d:none');
+    modalBackDrop.classList.remove("d:none");
 });
 
 // 모달 닫기 버튼 처리
@@ -13,4 +16,6 @@ let confirmBtn = document.getElementById('conf-btn');
 
 confirmBtn.addEventListener('click', () => {
     randModal.classList.add('d:none'); 
+    modalBackDrop.classList.add("d:none");
 });
+
